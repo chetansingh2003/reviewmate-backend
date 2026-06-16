12dd3495eb0bfa353013c5f61dba2c8ff93618bd
Chetan Singh <chetansingh2003@gmail.com>
2026-06-17 01:47:14 +0530

    update payy

--- server.js
+++ server.js
@@ -348,3 +348,32 @@ app.get("/my-business-test", async (req, res) => {
 
 });
 
+const Razorpay = require("razorpay");
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+app.post("/create-order", async (req, res) => {
+  try {
+
+    const { amount } = req.body;
+
+    const order = await razorpay.orders.create({
+      amount: amount,
+      currency: "INR",
+      receipt: `reviewmate_${Date.now()}`
+    });
+
+    res.json(order);
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
